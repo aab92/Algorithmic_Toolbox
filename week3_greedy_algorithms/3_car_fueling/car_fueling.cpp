f982866ad0f37a0e47e3ea00b361b038e4a9ab3f
Antonio Araujo <araujob.antonio@gmail.com>
2019-07-18 14:04:53 +0200

    week3 progress until assignment 4

--- week3_greedy_algorithms/3_car_fueling/car_fueling.cpp
+++ week3_greedy_algorithms/3_car_fueling/car_fueling.cpp
@@ -7,8 +7,26 @@ using std::vector;
 using std::max;
 
 int compute_min_refills(int dist, int tank, vector<int> & stops) {
-    // write your code here
-    return -1;
+    if (tank > dist) return 0;
+
+    int num_refill = 0;
+    int current_station = 0;
+    vector<int> station_refill_index;
+    auto iter = stops.begin();
+    iter =  stops.insert(iter, 0);
+    stops.push_back(dist);
+
+    for (int i=0; i<stops.size(); ++i){
+      if(stops[i+1]-stops[i] > tank)
+        return -1;
+      if(stops[i]-stops[current_station] > tank){
+              current_station = i-1;
+              num_refill ++;
+              station_refill_index.push_back(current_station);
+      }
+    }
+
+    return num_refill;
 }
 
 
--- week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.cpp
+++ week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.cpp
@@ -1,11 +1,44 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <cassert>
 
 using std::vector;
 
+void sort_vector(vector<int>& vectosort){
+  int size = vectosort.size();
+  vector<int> temparray(size);
+	vector<int> indexarr(size);
+  indexarr[0] = -1;
+
+  // Get maximum
+	for (int ex=0; ex<size; ex++){
+		int max = -1e6;
+		for (int i=0; i<size; i++){
+				bool condition = true;
+				for (int looper=1; looper < ex+1; looper++){
+					condition = condition && (i != indexarr[ex-looper]);
+				}
+				if (condition && (vectosort[i] > max)){
+						max = vectosort[i];
+						indexarr[ex] = i;
+				}
+		}
+		temparray[ex] = max;
+	}
+
+  for(int i=0; i<size; i++){
+    vectosort[i] = temparray[i];
+    //std::cout << vectosort[i] << " " << indexarr[i] << std::endl;
+  }
+}
+
+
 long long max_dot_product(vector<int> a, vector<int> b) {
-  // write your code here
+
+  sort_vector(a);
+  sort_vector(b);
+
   long long result = 0;
   for (size_t i = 0; i < a.size(); i++) {
     result += ((long long) a[i]) * b[i];
@@ -23,5 +56,8 @@ int main() {
   for (size_t i = 0; i < n; i++) {
     std::cin >> b[i];
   }
+
+  assert((a.size() == b.size()) && (a.size() == n));
+
   std::cout << max_dot_product(a, b) << std::endl;
 }
